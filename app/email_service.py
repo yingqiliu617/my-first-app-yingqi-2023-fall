2f8aa65f10d3f679fdf1e1de4a870edc7a46c9ac
yingqiliu617 <yl1410@georgetown.edu>
2023-11-15 16:02:03 -0500

    Ask for user email address

--- app/email_service.py
+++ app/email_service.py
@@ -40,7 +40,7 @@ def send_email(recipient_address=SENDER_ADDRESS, subject="[Shopping Cart App] Te
 
 
 
-
+user_address = input("Please enter your email address: ")
 
 
 my_content = """
@@ -61,4 +61,4 @@ my_content = """
         <li>Strawberry</li>
     </ul>
 """
-send_email(html_content=my_content)
+send_email(html_content=my_content, recipient_address=user_address)
