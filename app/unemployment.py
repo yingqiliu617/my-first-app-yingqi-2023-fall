60ea6cbb00c847295256798fa2ccb9bf9d8ddaeb
yingqiliu617 <yl1410@georgetown.edu>
2023-12-01 20:20:08 -0500

    Stocks functionality
    Stocks tests passed but some warnings pop up

--- app/unemployment.py
+++ app/unemployment.py
@@ -27,6 +27,16 @@ API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
 # FUNCTIONS
 
+def format_pct(my_number):
+    """
+    Formats a percentage number like 3.6555554 as percent, rounded to two decimal places.
+
+    Param my_number (float) like 3.6555554
+
+    Returns (str) like '3.66%'
+    """
+    return f"{my_number:.2f}%"
+
 def fetch_data():
 
     request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
